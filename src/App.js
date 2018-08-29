e3e289b3bb4d0bbdfa065460ca850bde8eeda681
Mohammed Abdulkereem <HorizonDragon@outlook.com>
2018-08-29 17:01:37 +0300

    Stopwatch in React

--- src/App.js
+++ src/App.js
@@ -3,7 +3,33 @@ import './App.css';
 
 class App extends Component {
   state = {
-    count: 0
+    time: 0
+  }
+
+  start() {
+    this.handle = setInterval(() => {
+      this.setState({
+        time: this.state.time + 10
+      })
+    }, 10);
+  }
+
+  pause() {
+    clearInterval(this.handle);
+  }
+
+  formatTime(number) {
+    const hours = Math.floor(number / 1000 / 60 / 60);
+    const minutes = Math.floor(number / 1000 / 60) % (3600);
+    const seconds = Math.floor(number / 1000) % 60;
+    const milliSeconds = number % 1000;
+
+    return `${('0' + hours).slice(-2)}:${('0' + minutes).slice(-2)}:${('0' + seconds).slice(-2)}.${('00' + milliSeconds).slice(-3)}`;
+  }
+
+  reset() {
+    this.pause();
+    this.setState({ time: 0 });
   }
 
   componentDidUpdate() {
@@ -15,10 +41,25 @@ class App extends Component {
   }
 
   shouldComponentUpdate(newProps, newState) {
-    return newState.count % 3 === 0;;
+    return true;
   }
 
   render() {
+    return (<div>
+      <h1>{this.formatTime(this.state.time)}</h1>
+      <div>
+        <button onClick={this.start.bind(this)} className="btn btn-success">
+          Start
+        </button>
+        <button onClick={this.pause.bind(this)} className="btn btn-warning">
+          Stop
+        </button>
+        <button onClick={this.reset.bind(this)} className="btn btn-danger">
+          Reset
+        </button>
+      </div>
+    </div>)
+
     return (
       <div className="container">
         <div className="row">
@@ -26,15 +67,18 @@ class App extends Component {
           <div className="col-md-4">
             <div className="row">
               <div className="col-md-12">
-                <h1>Count: {this.state.count}</h1>
+                <h1>{this.formatTime(this.state.time)}</h1>
               </div>
               <div className="col-md-12">
                 <div className="btn-group">
-                  <button onClick={this.increment} className="btn btn-success">
-                    Increment
+                  <button onClick={this.start.bind(this)} className="btn btn-success">
+                    Start
                   </button>
-                  <button onClick={this.decrement} className="btn btn-danger">
-                    Decrement
+                  <button onClick={this.pause.bind(this)} className="btn btn-warning">
+                    Stop
+                  </button>
+                  <button onClick={this.reset.bind(this)} className="btn btn-danger">
+                    Reset
                   </button>
                 </div>
               </div>
